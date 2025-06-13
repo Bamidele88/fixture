98584f38751a0fd282957e463b30481e9e5c1afb
Bamidele88 <adeyaleibrahim467@gmail.com>
2025-06-13 17:29:54 +0100

    Update fixtures.py

--- fixtures.py
+++ fixtures.py
@@ -2,9 +2,10 @@ import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as pt
 import numpy as np
-
+import seaborn as sns
 
 
 #import my csv file
+
 df = pd.read_csv("fixtures.csv")
-st.write(df.head())
+st.write(df.head(5))
